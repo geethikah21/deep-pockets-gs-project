bbc3c0be358729cfdac6c0f87b54ce36cfad8ab1
Geethika Hemkumar <geethikah21@gmail.com>
2021-02-28 21:40:14 -0600

    started line graph component, added recharts module

---
+++ eng-possibilities-ui-master/src/components/LineGraph/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import { Line, LineChart, CartesianGrid, XAxis, YAxis, Tooltip } from "recharts";
+
+const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}, {name: 'Page B', uv: 500, pv: 2500, amt: 2000}];
+
+export const LineGraph = () => {
+  return (
+    <>
+      <LineChart width={400} height={400} data={data}>
+        <Line type="monotone" dataKey="amt" stroke="#8884d8" />
+        <CartesianGrid stroke="#ccc" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+      </LineChart>
+    </>
+  );
+};
+
+// export default {LineGraph, data};
