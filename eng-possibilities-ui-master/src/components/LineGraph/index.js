32e76637125b5c7917c4e46d3a82b2ceb92687f0
Geethika Hemkumar <geethikah21@gmail.com>
2021-03-01 19:46:00 -0600

    LineGraph updates

--- eng-possibilities-ui-master/src/components/LineGraph/index.js
+++ eng-possibilities-ui-master/src/components/LineGraph/index.js
@@ -2,17 +2,22 @@ import React from "react";
 
 import { Line, LineChart, CartesianGrid, XAxis, YAxis, Tooltip } from "recharts";
 
-const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}, {name: 'Page B', uv: 500, pv: 2500, amt: 2000}];
-
-export const LineGraph = () => {
+export const LineGraph = (props) => {
+  const data = props.data.map((value, index) => {
+    return {
+      "name": `year ${index}`, 
+      value
+    }
+  });
+  console.log(data);
   return (
     <>
       <LineChart width={400} height={400} data={data}>
-        <Line type="monotone" dataKey="amt" stroke="#8884d8" />
         <CartesianGrid stroke="#ccc" />
-        <XAxis dataKey="name" />
+        <XAxis dataKey="year" />
         <YAxis />
         <Tooltip />
+        <Line type="monotone" dataKey="value" stroke="#8884d8" />
       </LineChart>
     </>
   );
--- eng-possibilities-ui-master/src/pages/Forecaster/index.js
+++ eng-possibilities-ui-master/src/pages/Forecaster/index.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import { LineGraph } from "../../components/LineGraph";
 
+const mock = [11506, 13260, 15428, 17959, 21077, 25041, 29787, 34898, 41382, 48824];
+
 const ForecasterHome = () => {
+  const [lineGraphData, setLineGraphData] = useState(mock);
+  const fetchForecast = () => {
+    // call setLineGraphData on response from fetch
+    fetch('http://example.com/movies.json')
+    .then(response => response.json())
+    .then(data => console.log(data));
+  };
+
   return (
     <>
        <div>
           <h3>Investment Forecaster</h3>
           <p>Logic goes here</p>
-          <LineGraph/>
+          <LineGraph data={lineGraphData}/>
         </div>
         
     </>
